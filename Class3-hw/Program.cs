5644b48ff7ebe50ef2c82704ea80cd1716bddae6
firathov <firat.hov@gmail.com>
2021-05-18 12:33:10 +0400

    firstCommit

---
+++ Class3-hw/Program.cs
@@ -0,0 +1,439 @@
+﻿using System;
+
+namespace Class3_hw
+{
+    abstract class Person
+    {
+        public string Surname { get; set; }
+        public int Course { get; set; }
+        public int GradeBook { get; set; }
+
+        Person[] data;
+        public Person()
+        {
+            data = new Person[1000000000];
+        }
+
+        public Person this[int index]
+        {
+            get
+            {
+                return data[index];
+            }
+            set
+            {
+                data[index] = value;
+            }
+        }
+        public Person(string surname, int course, int gradeBook)
+        {
+            Surname = surname;
+            Course = course;
+            GradeBook = gradeBook;
+        }
+        public abstract void Print();
+    }
+    class Student: Person
+    {
+        Person[] students;
+        public Student()
+        {
+            students = new Person[10000];
+        }
+
+        public Person this[int index]
+        {
+            get
+            {
+                return students[index];
+            }
+            set
+            {
+                students[index] = value;
+            }
+        }
+
+        public override void Print()
+        {
+            Console.WriteLine($"Студент - { Surname }, Курс - { Course }, Номер зачетной книги - { GradeBook }");
+        }
+
+    }
+
+    class Aspirant : Person
+    {
+        private string slideShow;
+
+        public string SlideShow
+        {
+            get
+            {
+                return slideShow;
+            }
+            set
+            {
+                slideShow = value;
+            }
+        }
+        Person[] aspirants;
+        public Aspirant()
+        {
+            aspirants = new Person[1000];
+        }
+        public Person this[int index]
+        {
+            get
+            {
+                return aspirants[index];
+            }
+            set
+            {
+                aspirants[index] = value;
+            }
+        }
+
+        public Aspirant(string slideShow, string surname, int course, int gradeBook)
+            : base(surname, course, gradeBook)
+        {
+            this.slideShow = slideShow;
+        }
+
+        public override void Print()
+        {
+                Console.WriteLine($"Аспирант - { Surname } Курс - { Course } Номер зачетной книги - { GradeBook } Название презентации - { SlideShow }");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student student = new Student();
+            Aspirant aspirant = new Aspirant();
+            int studentCounter = 0;
+            int aspirantCounter = 0;
+
+            for (; ; )
+            {
+                Menu();
+                Console.WriteLine("\nВыбирайте номер");
+                int switcher = forNumberCheck();
+                switch (switcher)
+                {
+                    case 1:
+                        {
+                            Console.WriteLine("Чтобы добавить студента, введите его/её фамилию");
+                            string surname = ForSurnameCheck();
+                            Console.WriteLine("Введите курс");
+                            int course = forCourseCheck();
+                            Console.WriteLine("Введите номер зачетной книги");
+                            int gradeBook = ForGradeBookCheck();
+                            student[studentCounter] = new Student { Surname = surname, Course = course, GradeBook = gradeBook };
+                            studentCounter++;
+                            Console.WriteLine("\nНажимайте на любую клавишу\n");
+                            break;
+                        }
+                    case 2:
+                        {
+                            Console.WriteLine("Чтобы добавить аспиранта, введите его/её фамилию");
+                            string surname = ForSurnameCheck();
+                            Console.WriteLine("Введите курс");
+                            int course = forCourseCheck();
+                            Console.WriteLine("Введите номер зачетной книги");
+                            int gradeBook = ForGradeBookCheck();
+                            Console.WriteLine("Введите название презентации");
+                            string slideShow = ForSurnameCheck();
+                            aspirant[aspirantCounter] = new Aspirant { Surname = surname, Course = course, GradeBook = gradeBook, SlideShow = slideShow };
+                            aspirantCounter++;
+                            Console.WriteLine("\nНажимайте на любую клавишу\n");
+                            break;
+                        }
+                    case 3:
+                        {
+                            Console.WriteLine($"Количесто студентов: {studentCounter}");
+                            Console.WriteLine("\nНажимайте на любую клавишу\n");
+                            break;
+                        }
+                    case 4:
+                        {
+                            Console.WriteLine($"Количество аспирантов: {aspirantCounter}");
+                            Console.WriteLine("\nНажимайте на любую клавишу\n");
+                            break;
+                        }
+                    case 5:
+                        {
+                            if (studentCounter == 0)
+                            {
+                                Console.WriteLine("Студентов нет");
+                            }
+                            for (int i = 0; i < studentCounter; i++)
+                            {
+                                if (student[i] == null)
+                                {
+                                    continue;
+                                }
+                                else
+                                {
+                                    Student human = (Student)student[i];
+                                    human.Print();
+                                }
+                            }
+                            Console.WriteLine("\nНажимайте на любую клавишу\n");
+                            break;
+                        }
+                    case 6:
+                        {
+                            if (aspirantCounter == 0)
+                            {
+                                Console.WriteLine("Аспирантов нет");
+                            }
+                            for (int i = 0; i < aspirantCounter; i++)
+                            {
+                                if (aspirant[i] == null)
+                                {
+                                    continue;
+                                }
+                                else
+                                {
+                                    Aspirant human = (Aspirant)aspirant[i];
+                                    human.Print();
+                                }
+                            }
+                            Console.WriteLine("\nНажимайте на любую клавишу\n");
+                            break;
+                        }
+                    case 7:
+                        {
+                            if (studentCounter == 0)
+                            {
+                                Console.WriteLine("Студентов нет");
+                                Console.WriteLine("\nНажимайте на любую клавишу\n");
+                                break;
+                            }
+                            Console.WriteLine("Для вывода студента, введите порядковый номер");
+                            int studentIndex = forNumberCheck() - 1;
+                            if (studentIndex >= studentCounter)
+                            {
+                                Console.WriteLine("Такого студента у нас нет");
+                            }
+                            else
+                            {
+                                Student human2 = (Student)student[studentIndex];
+                                if(student[studentIndex] == null)
+                                {
+                                    Console.WriteLine("Данный студент уже не учится у нас");
+                                }
+                                else
+                                {
+                                    Console.WriteLine($"Фамилия - {human2.Surname}, Курс - {human2.Course},Номер зачетной книги - {human2.GradeBook}");
+                                }
+                            }
+                            Console.WriteLine("\nНажимайте на любую клавишу\n");
+                            break;
+                        }
+                    case 8:
+                        {
+                            if (aspirantCounter == 0)
+                            {
+                                Console.WriteLine("Аспирантов нет");
+                                Console.WriteLine("\nНажимайте на любую клавишу\n");
+                                break;
+                            }
+                            Console.WriteLine("Для вывода аспиранта, введите порядковый номер");
+                            int aspirantIndex = forNumberCheck() - 1;
+                            if (aspirantIndex >= aspirantCounter)
+                            {
+                                Console.WriteLine("У нас нет такого аспиранта");
+                            }
+                            else
+                            {
+                                Aspirant human4 = (Aspirant)aspirant[aspirantIndex];
+                                if(aspirant[aspirantIndex] == null)
+                                {
+                                    Console.WriteLine("Данный аспирант у нас уже не учится");
+                                }
+                                else
+                                {
+                                    Console.WriteLine($"Фамилия - {human4.Surname}, Курс - {human4.Course},Номер зачетной книги - {human4.GradeBook}, Название презентации - {human4.SlideShow}");
+                                }
+                            }
+                            Console.WriteLine("\nНажимайте на любую клавишу\n");
+                            break;
+                        }
+                    case 9:
+                        {
+                            if (studentCounter == 0)
+                            {
+                                Console.WriteLine("Студентов нет");
+                                Console.WriteLine("\nНажимайте на любую клавишу\n");
+                                break;
+                            }
+                            Console.WriteLine("Для удаления студента, введите его порядковый номер");
+                            int deletteStudent = forNumberCheck() - 1;
+                            if (deletteStudent >= studentCounter)
+                            {
+                                Console.WriteLine("У нас нет такого студента");
+                                Console.WriteLine("\nНажимайте на любую клавишу\n");
+                                break;
+                            }
+                            else
+                            {
+                                Student human3 = (Student)student[deletteStudent];
+                                Console.WriteLine($"Студент {human3.Surname} под порядковым номером {deletteStudent + 1} удалён");
+                                student[deletteStudent] = null;
+                            }
+                            Console.WriteLine("\nНажимайте на любую клавишу\n");
+                            break;
+                        }
+                    case 10:
+                        {
+                            if (aspirantCounter == 0)
+                            {
+                                Console.WriteLine("Аспирантов нет");
+                                Console.WriteLine("\nНажимайте на любую клавишу\n");
+                                break;
+                            }
+                            Console.WriteLine("Для удаления аспиранта, введите его порядковый номер");
+                            int deletteAspirant = forNumberCheck() - 1;
+                            if (deletteAspirant >= aspirantCounter)
+                            {
+                                Console.WriteLine("У нас нет такого аспиранта");
+                            }
+                            else
+                            {
+                                Aspirant human5 = (Aspirant)aspirant[deletteAspirant];
+                                Console.WriteLine($"Аспирант {human5.Surname} под порядковым номером {deletteAspirant + 1} удалён");
+                                aspirant[deletteAspirant] = null;
+                            }
+                            Console.WriteLine("\nНажимайте на любую клавишу\n");
+                            break;
+                        }
+                    case 11:
+                        {
+                            Console.WriteLine("Удачи (ненавижу индексаторы)");
+                            break;
+                        }
+                    default:
+                        {
+                            Console.WriteLine("Такой цифры нет, выбирайте с 1-11: ");
+                            break;
+                        }
+                }
+                Console.ReadKey();
+            }
+
+        }
+
+        public static void Menu()
+        {
+            Console.WriteLine("1)Добавить Студента");
+            Console.WriteLine("2)Добавить Аспиранта");
+            Console.WriteLine("3)Вывести количество студентов");
+            Console.WriteLine("4)Вывести количество аспирантов");
+            Console.WriteLine("5)Вывести список всех студентов");
+            Console.WriteLine("6)Вывести список всех аспирантов");
+            Console.WriteLine("7)Вывести студента по порядковому индексу");
+            Console.WriteLine("8)Вывести аспиранта по порядковому индексу");
+            Console.WriteLine("9)Удаление студента по порядковому индексу");
+            Console.WriteLine("10)Удаление аспиранта по порядковому индексу");
+            Console.WriteLine("11)Exit");
+        }
+
+        public static bool surnameCompare = true;
+
+        static string ForSurnameCheck()
+        {
+            string humanSurname;
+            do
+            {
+                humanSurname = Console.ReadLine();
+
+                for (int i = 0; i < humanSurname.Length; i++)
+                {
+                    char element = humanSurname[i];
+
+                    if (!Char.IsLetter(element))
+                    {
+                        surnameCompare = false;
+                        Console.Write("Вы ввели неправильный символ, попробуйте ещё: ");
+                        break;
+                    }
+                    else
+                    {
+                        surnameCompare = true;
+                    }
+                }
+                if (humanSurname.Length < 1)
+                {
+                    Console.WriteLine("Имя не может быть меньше 1 трёх букв, попробуйте ещё");
+                    surnameCompare = false;
+                }
+
+
+            }
+            while (surnameCompare == false);
+            return humanSurname;
+        }
+
+        static int ForGradeBookCheck()
+        {
+            int number;
+
+            for (; ; )
+            {
+                string checkingNumber = Console.ReadLine();
+                number = 0;
+                Int32.TryParse(checkingNumber, out number);
+
+                if (number > 0 && number < 10000)
+                {
+                    return number;
+                }
+                else
+                {
+                    Console.Write("Вы ввели неправильно, введите от 1 до 9999: ");
+                }
+            }
+        }
+
+        static int forCourseCheck()
+        {
+            int number;
+
+            for (; ; )
+            {
+                string checkingNumber = Console.ReadLine();
+                number = 0;
+                Int32.TryParse(checkingNumber, out number);
+
+                if (number > 0 && number < 9)
+                {
+                    return number;
+                }
+                else
+                {
+                    Console.Write("Такого курса у нас нет, введите от 1 до 8: ");
+                }
+            }
+        }
+
+        static int forNumberCheck()
+        {
+            int number;
+
+            for (; ; )
+            {
+                string checkingNumber = Console.ReadLine();
+                ;
+                if (Int32.TryParse(checkingNumber, out number) && number > 0)
+                {
+                    return number;
+                }
+                else
+                {
+                    Console.Write("Вы ввели неправильный символ, попробуйте ещё: ");
+                }
+            }
+        }
+    }
+}
